50f26d50e1b2255bc0054a7a594d67848e15a5de
Ayoubb <148903995+Ayoubb4@users.noreply.github.com>
2024-10-03 16:52:42 +0200

    Update Ejercicio3.js

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -1,22 +1,31 @@
 /* 3- Crea una función que, dada una cadena de texto, devuelve una nueva cadena cambiando la primera letra de  cada palabra en mayúsculas. (3 puntos) 
  */
-function MayusculaPrimeraLetra(texto) {
+function MayusculaPrimeraLetra() {
+    let texto = prompt ("Dame una serie de palabras");
     let nuevaCadena = '';
 
     for (let i = 0; i < texto.length; i++) {
         let char = texto[i];
-
-        if (char == ' ' || char == '.') {
-            nuevaCadena += char;
-        } else if (char !== ' ') {
-            nuevaCadena += char.toUpperCase(); 
-        } else {
+        
+        if(texto[i] == texto[0]){
+        nuevaCadena+= texto[i].toUpperCase();
+        
+        }else if (char == ' ' || char == '.' || char == '. ') {
+            nuevaCadena += char ;
+            nuevaCadena += texto[i + 1].toUpperCase();
+            i++;
+        }/* lse if(char == '. '){
+            nuevaCadena += char ;
+            nuevaCadena += texto[i + 1].toUpperCase();
+            i++;
+        } */
+        else {
             nuevaCadena += char; 
-        }
+        }   
     }
 
     return nuevaCadena;
 }
 
-let texto = prompt ("Dame una serie de palabras");
-console.log(MayusculaPrimeraLetra(texto));
+let solucion = MayusculaPrimeraLetra();
+console.log(solucion);
